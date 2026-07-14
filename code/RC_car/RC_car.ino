70e800891f3637c7284df358e938ecaca2976d0f
rohan-sheikh05 <rohansheikh2005@gmial.com>
2026-07-14 12:21:09 +0600

    Initial commit

---
+++ code/RC_car/RC_car.ino
@@ -0,0 +1,93 @@
+// Bluetooth Controlled Car using Arduino nano + L298N + HC-05
+
+// Motor driver pins (L298N IN1–IN4)
+int IN1 = 2;
+int IN2 = 4;
+int IN3 = 6;
+int IN4 = 7;
+
+// Enable pins for speed control (PWM)
+int ENA = 5;  // Left motors
+int ENB = 10;   // Right motors
+
+
+
+// Variable to store Bluetooth command
+char command;
+
+void setup() {
+  // Motor pins
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+
+  Serial.begin(9600);
+
+  stopMotors();
+  digitalWrite(LED, LOW);
+}
+
+void loop() {
+  if (Serial.available()) {
+    command = Serial.read();
+
+    switch (command) {
+      case 'F': forward(); break;
+      case 'B': backward(); break;
+      case 'L': turnLeft(); break;
+      case 'R': turnRight(); break;
+      case 'S': stopMotors(); break;
+      default: stopMotors(); break;
+    }
+  }
+}
+
+// --- Motor Control Functions ---
+void forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 200); // speed (0–255)
+  analogWrite(ENB, 200);
+}
+
+void backward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 200);
+  analogWrite(ENB, 200);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 200);
+  analogWrite(ENB, 200);
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 200);
+  analogWrite(ENB, 200);
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+}
